59b6fa5b996f2ab40a69d21c68b9730a7f27bcb7
fretbuzz <JosephSeverini2017@u.northwestern.edu>
2015-11-21 22:23:08 -0600

    Attempting liftoff2

--- app/src/main/java/wildhacks2015v2/referencefinder/MyActivity.java
+++ app/src/main/java/wildhacks2015v2/referencefinder/MyActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +18,7 @@ import org.apache.commons.io.IOUtils;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -60,6 +62,9 @@ public class MyActivity extends AppCompatActivity {
 
         }
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
